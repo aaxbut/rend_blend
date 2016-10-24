096b07f3835f4e711e635e3919016cd1b79309f9
Andrey A.But <aaxbut@gmail.com>
2016-10-24 14:18:18 +0500

    set format

--- web_blend_service_v04.py
+++ web_blend_service_v04.py
@@ -195,11 +195,13 @@ def worker(q,task):
             bpy.context.scene.cycles.device='CPU'
             bpy.context.scene.render.ffmpeg.format = 'MPEG4'
 
-            bpy.context.scene.frame_start = 100
-            bpy.context.scene.frame_end = 300
+           # bpy.context.scene.frame_start = 100
+           # bpy.context.scene.frame_end = 300
             #os.chown(bpy.context.scene.render.filepath, 500, 500)
             bpy.ops.render.render(animation=True,scene=bpy.context.scene.name)
             os.chown(bpy.context.scene.render.filepath, 500, 500)
+            os.chmod(bpy.context.scene.render.filepath, 766)
+
             #logging.info(' ###########{} ###################: render  name {} '.format(g,task['project_name']))
            # yield from p
             #    logging.info(' {} ###################: render  name {} path {}: {}'.format(q.get(),task['project_name'],datetime.now().strftime('%c'),o))
